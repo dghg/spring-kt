1c421ea79f24ae77f3340d7aa9d7a7b5dc4b1943
Donghyeong Kang <dh@uos.ac.kr>
2022-10-01 23:22:25 +0900

    first commit

---
+++ myapp/src/test/kotlin/com/github/dghg/myapp/MyappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dghg.myapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
